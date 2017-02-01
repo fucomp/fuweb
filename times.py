4fd6a4568c4e556ad9a730cf988a6bba606a2ade
Oholiab <oholiab@grimmwa.re>
2017-02-01 18:24:51 +0000

    Can read an article on the times

--- times.py
+++ times.py
@@ -6,18 +6,21 @@ import os
 import argparse
 import names
 import time
+import random
+import string
 
+def randomword(length):
+    return ''.join(random.choice(string.lowercase) for i in range(length))
 
 firstname = names.get_first_name()
 lastname = names.get_last_name()
+email = ''.join([randomword(10), '@', randomword(10), '.com'])
+password = randomword(16)
 
 print(' '.join([firstname, lastname, "has decided to sign up for thetimes.co.uk"]))
 
 pwd = os.path.dirname(os.path.realpath(__file__))
-
 path_to_chromedriver = pwd + '/chromedriver'
-print(path_to_chromedriver)
-
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 url = "http://www.thetimes.co.uk/edition/news/rip-off-rail-fares-scrapped-3vdkqtckw"
@@ -37,3 +40,10 @@ def retry_for_slow_elements(f, args):
         break
 
 retry_for_slow_elements(click_on_element, "popup-get-access")
+time.sleep(5)
+browser.find_element_by_id("firstName").send_keys(firstname)
+browser.find_element_by_id("lastName").send_keys(lastname)
+browser.find_element_by_id("email").send_keys(email)
+browser.find_element_by_id("confirmEmail").send_keys(email)
+browser.find_element_by_id("password").send_keys(password)
+browser.find_element_by_id("accountPopSubmit").click()
